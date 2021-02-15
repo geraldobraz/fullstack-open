f898c4bb18953927c2d7b38f275943b26d2c05c0
Geraldo Braz <gban50@gmail.com>
2021-02-15 14:35:21 -0300

    Exercise 1.5

--- part1/src/index.js
+++ part1/src/index.js
@@ -6,27 +6,29 @@ import Total from './components/Total/Total';
 import './index.css';
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      },
+    ],
+  };
   
   return (
     <>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   );
 };
