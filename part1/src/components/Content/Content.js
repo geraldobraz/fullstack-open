9618fac3f42eb1d830353ffc8808459506ae5d97
Geraldo Braz <gban50@gmail.com>
2021-02-15 14:33:33 -0300

    Exercise 1.4

--- part1/src/components/Content/Content.js
+++ part1/src/components/Content/Content.js
@@ -4,9 +4,9 @@ import Part from '../Part/Part';
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </>
   );
 }
