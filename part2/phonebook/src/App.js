020b55e5ac9b1ead9beeddb2da03c90f937dc5ac
Geraldo Braz <gban50@gmail.com>
2021-03-16 21:35:02 -0300

    Exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,16 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([]);
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
+  
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
+  const [ searchedName, setSearchedName ] = useState('');
 
   const onFormSubmit = (e) => {
     e.preventDefault();
@@ -29,10 +36,24 @@ const App = () => {
   const handleNumberChange = (e) => {
     setNewNumber(e.target.value);
   };
+  
+  const handleSearchPersons = (e) => {
+    setSearchedName(e.target.value);
+  };
+
+  const personsToShow = useMemo(() => {
+    if (searchedName === '') {
+        return persons;
+    } else {
+      return persons.filter(person => person.name.includes(searchedName));
+    }
+  }, [persons, searchedName]);
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <input value={searchedName} onChange={handleSearchPersons}/>
+
       <form onSubmit={onFormSubmit}>
         <div>
           name: <input value={newName} onChange={handlePersonChange}/>
@@ -45,7 +66,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, i) => <div key={i}>{person.name} {person.number}</div>)}
+      {personsToShow.map((person, i) => <div key={i}>{person.name} {person.number}</div>)}
     </div>
   )
 }
