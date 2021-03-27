863c8f04d6a13a16309fe39b2953e9708a9358a2
Geraldo Braz <geraldo_neto@apple.com>
2021-03-26 22:07:00 -0300

    Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,11 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import api from './services/api';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [ persons, setPersons ] = useState([]);
   
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
@@ -51,6 +47,13 @@ const App = () => {
       return persons.filter(person => person.name.toLowerCase().includes(searchedName));
     }
   }, [persons, searchedName]);
+
+
+  useEffect(() => {
+    api.get('/persons').then(({ data }) => {
+      setPersons(data)
+    });
+  }, []);
 
   return (
     <div>
